d04288f4aab2d141607ab7b64019e8dbb28d20f2
Yoana Bastiyanova <bastianovajoana@gmail.com>
2026-05-18 14:32:34 +0300

    started working on base.html

--- user_tasks/views.py
+++ user_tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+
+from user_tasks.models import UserTaskTracker
+
 
 # Create your views here.
+class DashboardView(ListView):
+    model = UserTaskTracker
+    template_name = "dashboard.html"
+    context_object_name = "dashboard_tasks"
